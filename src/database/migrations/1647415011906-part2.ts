62759be731c0750963e3ce518dc0004ae511395b
Artyom Tarasov <artem.tarasov@softdev.pw>
2022-03-17 12:23:22 +0600

    migrate without loosing data

--- src/database/migrations/1647415011906-part2.ts
+++ src/database/migrations/1647415011906-part2.ts
@@ -6,16 +6,13 @@ export class part21647415011906 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "content" DROP COLUMN "href"`);
     await queryRunner.query(
-      `ALTER TABLE "content" ADD "groupId" character varying NOT NULL`
+      `ALTER TABLE "content" ADD "groupId" character varying`
     );
     await queryRunner.query(
       `CREATE TABLE "content_group" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "userId" uuid NOT NULL, "type" character varying NOT NULL, CONSTRAINT "PK_b459ae1ffef2a947ebe7f06f0bf" PRIMARY KEY ("id"))`
     );
     await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "password"`);
-    await queryRunner.query(`ALTER TABLE "content" DROP COLUMN "groupId"`);
-    await queryRunner.query(
-      `ALTER TABLE "content" ADD "groupId" uuid NOT NULL`
-    );
+    await queryRunner.query(`ALTER TABLE "content" ADD "groupId" uuid`);
     await queryRunner.query(
       `ALTER TABLE "content" ADD CONSTRAINT "FK_b41b839c1ad37c88100711f4441" FOREIGN KEY ("groupId") REFERENCES "content_group"("id") ON DELETE CASCADE ON UPDATE CASCADE`
     );
@@ -25,22 +22,8 @@ export class part21647415011906 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "content_group" DROP CONSTRAINT "FK_098035d32a04f45d75005bb9cf2"`
-    );
-    await queryRunner.query(
-      `ALTER TABLE "content" DROP CONSTRAINT "FK_b41b839c1ad37c88100711f4441"`
-    );
-    await queryRunner.query(`ALTER TABLE "content" DROP COLUMN "groupId"`);
-    await queryRunner.query(
-      `ALTER TABLE "content" ADD "groupId" character varying NOT NULL`
-    );
-    await queryRunner.query(
-      `ALTER TABLE "user" ADD "password" character varying NOT NULL`
-    );
-    await queryRunner.query(`DROP TABLE "content_group"`);
-    await queryRunner.query(
-      `ALTER TABLE "content" RENAME COLUMN "groupId" TO "href"`
+    throw new Error(
+      "Not implemented. There is no way to revert migration without loosing data"
     );
   }
 }
